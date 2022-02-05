fe01df7f1914435ac3f9482ad91b9f8876a33e8a
truc0 <22969604+truc0@users.noreply.github.com>
2022-02-05 15:11:43 +0000

    feat: add basic function with very basic interface

---
+++ src/apis/index.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+import { API_BASE_URL } from '@/config';
+import { Station } from '@/types/station';
+
+interface IndexResponse {
+    data: [Station];
+    total: number;
+}
+
+export function fetchStations(): Promise<IndexResponse> {
+    return axios.get<IndexResponse>(API_BASE_URL)
+            .then(response => response.data);
+}
